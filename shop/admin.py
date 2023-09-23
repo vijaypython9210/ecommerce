c72232d744165f9ee92170d6fbb4508be70aef19
vijaypython9210 <134040830+vijaypython9210@users.noreply.github.com>
2023-09-23 13:04:12 +0530

    Add files via upload

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 # Register your models here.
 # class CategoryAdmin(admin.ModelAdmin):
 #     list_display=('name','image','description')
 
 # admin.site.register(Category,CategoryAdmin)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(Favourite)
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
+admin.site.register(Product,ProductAdmin)
+
+class CartAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
+admin.site.register(Cart,CartAdmin)
+
+class FavouriteAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
+admin.site.register(Favourite,FavouriteAdmin)
