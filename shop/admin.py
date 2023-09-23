fcf16f827bc561fcd9e4e1f06caac80c8f4e0bbb
vijaypython9210 <nvijay9210@gmail.com>
2023-09-23 12:54:02 +0530

    import_export

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 # Register your models here.
 # class CategoryAdmin(admin.ModelAdmin):
 #     list_display=('name','image','description')
 
 # admin.site.register(Category,CategoryAdmin)
-admin.site.register(Category)
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+        ...
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(Favourite)
